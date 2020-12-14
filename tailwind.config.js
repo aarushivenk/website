5a14a13ed8d827b80de3ee9680f8af36ed12d139
Aarushi Venkatakrishnan <aarushi@aarushis-mbp.lan>
2020-12-13 23:21:41 -0500

    black and white layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'display': ['Spartan'],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
